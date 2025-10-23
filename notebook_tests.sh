1a51888a7069eb4fd023bd851353cac478c863c4
Timotej Bernat <tibe3324@colorado.edu>
2025-10-22 21:37:13 -0600

    Added basic and advanced "from scratch" assembly notebooks to nbval test script

--- notebook_tests.sh
+++ notebook_tests.sh
@@ -4,6 +4,7 @@ py.test --nbval-lax --current-env -vv \
     mupt_shapes.ipynb \
     connector_alignment.ipynb \
     mupt_primitives_rdkit.ipynb \
-    mol_from_scratch.ipynb \
+    mol_from_scratch_basic.ipynb \
+    mol_from_scratch_advanced.ipynb \
     ellipsoidal_chain_placement.ipynb \
     tripeptide.ipynb \
